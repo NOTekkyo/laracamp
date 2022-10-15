2aa18fbe6151edea85e870021cf84e129e41a6e1
NOTekkyo <jonathan.422021011@civitas.ukrida.ac.id>
2022-10-15 13:04:35 +0700

    Migration: users, camps, camp_benefits, checkouts

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,9 +18,12 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('password')->nullable(); // not required when using OAuth
+            $table->string('occupation')->nullable();
+            $table->boolean('is_admin')->default(false);
+            $table->rememberToken(); //on login will update
+            $table->timestamps(); //will create created_At & update_At. These field will autofilled on addition/change 
+            $table->softDeletes(); // for deleted_at
         });
     }
 
